e0a36cbadbbe37e68c9457de0fa6f02f9999fc20
Thilina Vithana <thilinavithana98@gmail.com>
2023-09-22 16:50:03 +0530

    Added new configuration in updated.config.js

--- updated.config.js
+++ updated.config.js
@@ -36,6 +36,10 @@ window.STOP_MIC_TRACK_ON_MUTE = true;
 // in json format
 window.DESIGN_CUSTOMIZATION = `{
   "background_color": "#F9F9F9",
+  "background_image": "",
+  "left_side_bg_color": "#FFFFFF",
+  "right_side_bg_color": "#FFFFFF",
+  "custom_css_url": "",
   "custom_logo": "https://digitalservices.lk/wp-content/uploads/2021/04/DIGI-LOGO-2.png"
 }`;
 
